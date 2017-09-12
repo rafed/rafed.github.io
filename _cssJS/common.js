4582122cf00f67086c02f54fd3b1be9e347cb15b
Rafed <rafed123@yahoo.com>
2017-09-13 01:58:22 +0600

    Added google analytics (unactivated)

--- _cssJS/common.js
+++ _cssJS/common.js
@@ -2,12 +2,7 @@
 permalink: common.js
 ---
 
-function socialMediaPopUp(url, title, width, height) {
-	var left = (screen.width/2)-(width/2);
-	var top = (screen.height/2)-(height/2);
-	window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, copyhistory=no, width='+ width +', height='+ height +', top=' + top + ', left=' + left);
-} 
-
+// Google custom search
 (function() {
 	var cx = '000151824860492892407:kd5zijxwj6a';
 	var gcse = document.createElement('script');
@@ -29,4 +24,19 @@ function executeQuery() {
 	return false;
 }
 
+// Sharing
+function socialMediaPopUp(url, title, width, height) {
+	var left = (screen.width/2)-(width/2);
+	var top = (screen.height/2)-(height/2);
+	window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, copyhistory=no, width='+ width +', height='+ height +', top=' + top + ', left=' + left);
+} 
 
+/*
+// Google analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  
+	ga('create', 'UA-106132552-1', 'auto');
+	ga('send', 'pageview'); */
